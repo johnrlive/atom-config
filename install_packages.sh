4ce7246009d2ed36cb9f164cf33b550bb122f3ef
John <john@addlocal.com>
2016-06-28 13:13:46 -0400

    init commit

---
+++ install_packages.sh
@@ -0,0 +1,62 @@
+#!bin/bash
+
+# system settings
+apm install vim-mode-plus
+apm install symbols-tree-view
+apm install auto-detect-indentation
+apm install highlight-selected
+apm install emmet
+apm install foldingtext-for-atom
+apm install clipboard-plus
+
+
+# File settings
+apm install file-icons
+apm install advanced-open-file
+apm install open-recent
+apm install project-manager
+apm install project-plus
+apm install expose
+
+
+#productivity
+apm install imdone-atom #A task-board for TODOs, FIXMEs, HACKs, etc in your code.
+apm install jumpy
+apm install toggle-quotes
+
+# Minimap settings
+apm install minimap
+apm install minimap-autohide
+apm install minimap-codeglance
+apm install minimap-cursorline
+apm install minimap-find-and-replace
+apm install minimap-hide
+apm install minimap-highlight-selected
+
+# HTML Packages
+apm install less-than-slash
+
+# Color
+apm install pigments
+apm install color-picker
+
+# Linter Packages
+apm install linter
+apm install linter-htmlhint
+apm install linter-csshint
+apm install linter-jshint
+apm install linter-flake8
+
+# Django Packages
+apm install atom-django
+
+# python settings
+pip install --upgrade flake8 flake8-docstrings jedi yapf
+apm install atom-beautify
+
+# Terminal Packages
+npm install -g node-gyp
+apm install terminal-plus
+
+# misc.
+apm install rest-client
